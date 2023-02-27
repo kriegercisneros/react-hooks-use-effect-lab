4ea34dbd7236ace52ba54923fd53db6a2840ff61
JackieCisneros <krieger.jacqueline@gmail.com>
2023-02-27 11:46:04 -0700

    here is the completed lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState}from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
+  useEffect(() => {
+    if(timeRemaining === 0){
+      setTimeRemaining(10); 
+      onAnswered(false);
+      return; //this will exit the useEffect function early--
+      //??How do I know this is the right option??
+    }
+  
 
-  // add useEffect code
+    // set a timeout to run after 1 second 
+    const timerId = setTimeout(() =>{
+      //this is to decrement the time remaining, but why does it need a callbackfunction?  
+      setTimeRemaining((timeRemaining)=> timeRemaining - 1 )
+    }, 1000);
+
+  //clean up after the timeout incase the component unmounts before the
+  //timer is done
+    return function (){
+      clearTimeout(timerId)
+    }
+  }, [timeRemaining, onAnswered])
+  //we want to run the useEffect function every time timeRemaining changes
+  //onAnsered is also a dependency eventhough it doesn't change
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
